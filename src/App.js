df45818b954b7a1c2b4eb7325e3d2e6579035626
Devbrotho Nath <devbrotho.dn@gmail.com>
2023-04-24 17:37:57 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <ul>
         <li>Click on the heading tag of any column to sort the values by ascending or descending.</li>
         <li>Use the Table Search for searching any value inside the table.</li>
-        <li>Use the Search bar inside every column to do a column search.</li>
+        <li>Use the Search bar inside any column to do a column search.</li>
       </ul>
       <Table />
     </div>
