92d796510ddf4cd5b033fa833f3425c94028f8fa
Devbrotho Nath <devbrotho.dn@gmail.com>
2023-04-24 17:19:40 +0530

    Added instructions

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import { Table } from './components/Table'
 function App() {
   return (
     <div className='App'>
+      <ul>
+        <li>Click on the heading tag of any column to sort the values by ascending or descending.</li>
+        <li>Use the Table Search for searching any value inside the table.</li>
+        <li>Use the Search bar inside every column to do a column search.</li>
+      </ul>
       <Table />
     </div>
   )
